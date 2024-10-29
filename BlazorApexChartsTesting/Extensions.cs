d3f04e76f861d1e81b17e978be8864a2d4064051
DOMAIN\mhampton <mhampton@dolbey.com>
2024-10-29 15:30:20 -0400

    Initial Commit

---
+++ BlazorApexChartsTesting/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace BlazorApexChartsTesting;
+
+public static class Extensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string ToJson(this object obj)
+    => System.Text.Json.JsonSerializer.Serialize(obj);
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BlazorApexChartsTesting
+
 Testing and bug investigation for Blazor Apex Charts.
+
+Current bug testing: Index page shows issue with clicking and hovering on line charts.
